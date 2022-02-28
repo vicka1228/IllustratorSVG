82c3e4339c7783195554efecc9c712cd5c700247
Bhavicka Mohta <91835014+vicka1228@users.noreply.github.com>
2022-03-01 01:20:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let moon = document.getElementByID("Moon")
 moon.addEventListener("click", () => {
   document.getElementByID("textbox").innerText = "I am the moon.";
 })
+
+let ursaMajor = document.getElementByID("Ursa Major")
+
+ursaMajor.addEventListener("click", () => {
+  document.getElementByID("textbox").innerText = "I am Ursa Major.";
+})
+
+let ursaMinor = document.getElementByID("Ursa Minor")
+
+ursaMinor.addEventListener("click", () => {
+  document.getElementByID("textbox").innerText = "I am Ursa Minor.";
+})
